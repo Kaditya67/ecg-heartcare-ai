a3593de5da38c6ba72bbfcb0deee3a979ac0ca49
kaditya67 <322adityakumar0004@dbit.in>
2025-08-16 12:51:01 +0530

    Migrated and created User

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import ECGFile, ECGRecord
 
 # Register your models here.
+admin.site.site_header = "ECG Labeling System Admin"
+admin.site.site_title = "ECG Labeling System Admin Portal"
+
+admin.site.index_title = "Welcome to the ECG Labeling System Admin Portal"
+
+admin.site.register(ECGFile)    
+admin.site.register(ECGRecord)
